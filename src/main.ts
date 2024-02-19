0831205440f3e1ace2b27ad2525425ff4500aa05
gnomedevreact <143091438+gnomedevreact@users.noreply.github.com>
2024-02-19 17:04:20 +0300

    patch:Memory

--- src/main.ts
+++ src/main.ts
@@ -17,15 +17,6 @@ async function bootstrap() {
     exposedHeaders: "set-cookie",
   });
 
-  const config = new DocumentBuilder()
-    .setTitle("Cats example")
-    .setDescription("The cats API description")
-    .setVersion("1.0")
-    .addTag("cats")
-    .build();
-  const document = SwaggerModule.createDocument(app, config);
-  SwaggerModule.setup("swagger", app, document);
-
   await app.listen(port);
 }
 bootstrap();
